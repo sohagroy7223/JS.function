3cf1eba5e717f193f6ec9fc9ae38284114cb19df
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-16 13:41:03 +0600

    addad argument modul js file

---
+++ arguments.js
@@ -0,0 +1,32 @@
+function numbers(num1) {
+    // console.log(num1);
+    const doboll = num1 * 2;
+    console.log(doboll);
+}
+numbers(23);
+
+console.log("------------");
+
+numbers(3);
+
+console.log("-------");
+
+const number = 50;
+numbers(number);
+
+console.log("--------");
+
+const sonkha = 30;
+numbers(sonkha);
+
+console.log("--------");
+
+function tofat(father, son) {
+    console.log(father, son);
+    const dif = father - son;
+    console.log(dif);
+}
+tofat(50, 20);
+const fatherage = 60;
+const myage = 28;
+tofat(fatherage, myage);
