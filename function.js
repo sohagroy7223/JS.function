e6468bb40cbf536f695bc1d261b5c0239e89f691
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-16 11:44:15 +0600

    addad function modul in js file

---
+++ function.js
@@ -0,0 +1,9 @@
+function dimpose(tal, lobon, holot, paij) {
+    console.log("1 st tal gorom kore neta hobe");
+    console.log("dim, lobon,and paij mix korte hobe"),
+        console.log("then korita care deta hobe");
+
+    return "akta delecious dim poge toire hobe";
+}
+const result = dimpose("dal", "vaji");
+console.log(result);
---
+++ parametar.js
@@ -0,0 +1,27 @@
+function squre(a) {
+    console.log(a);
+    const goun = a * a;
+    console.log(goun);
+}
+// call the function
+// squre(4);
+// squre(405);
+
+function dimpoj() {
+    console.log("1st tal gorm korte hobe");
+    console.log("dim , holut , paij, add koro");
+    console.log("dim korai ta care dao");
+
+    return "dim poje ready ";
+}
+// call the function
+// const result = dimpoj("elish", "alu", "vajitable");
+// console.log(result);
+
+function add(a, b) {
+    console.log(a, b);
+    const jog = a * b;
+    console.log(jog);
+}
+// call the function
+add(5, 10);
