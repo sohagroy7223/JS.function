44644c08c9d37adb8aab650bc6c45b2578c3f34b
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-16 21:40:55 +0600

    addad return modul in js file

--- arguments.js
+++ arguments.js
@@ -1,30 +1,30 @@
-function numbers(num1) {
-    // console.log(num1);
-    const doboll = num1 * 2;
-    console.log(doboll);
-}
-numbers(23);
+// function numbers(num1) {
+//     // console.log(num1);
+//     const doboll = num1 * 2;
+//     console.log(doboll);
+// }
+// numbers(23);
 
-console.log("------------");
+// console.log("------------");
 
-numbers(3);
+// numbers(3);
 
-console.log("-------");
+// console.log("-------");
 
-const number = 50;
-numbers(number);
+// const number = 50;
+// numbers(number);
 
-console.log("--------");
+// console.log("--------");
 
-const sonkha = 30;
-numbers(sonkha);
+// const sonkha = 30;
+// numbers(sonkha);
 
-console.log("--------");
+// console.log("--------");
 
 function tofat(father, son) {
-    console.log(father, son);
+    // console.log(father, son);
     const dif = father - son;
-    console.log(dif);
+    console.log(father, son, "different is", dif);
 }
 tofat(50, 20);
 const fatherage = 60;
---
+++ return.js
@@ -0,0 +1,17 @@
+function sevenTime(number) {
+    const result = number * 7;
+    return result;
+}
+
+console.log("-----------");
+const fivtime = sevenTime(20);
+console.log(fivtime);
+console.log("---------------");
+const result = sevenTime(50);
+// console.log(result);
+
+function numhalf(num) {
+    const half = num / 2;
+    console.log(half);
+}
+numhalf(sevenTime(100));
