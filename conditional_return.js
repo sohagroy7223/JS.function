33a0dda1bed04554707176049f9608e1f4af09e6
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-17 12:13:43 +0600

    addad conditional return in js file

---
+++ conditional_return.js
@@ -0,0 +1,26 @@
+// function isEven(number) {
+//     if (number % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// const result = isEven(7);
+// console.log(result);
+// console.log("---------");
+
+// function isOdd(num) {
+//     const bijor = num % 2 === 0;
+//     console.log("ai sonkhs ta bijor", num, bijor);
+// }
+// isOdd(245);
+console.log("---------");
+
+function evenNum(num1) {
+    if (num1 % 2 === 0) {
+        console.log("it is jor", num1);
+    } else {
+        console.log("it is not even number.", num1);
+    }
+}
+evenNum(347);
