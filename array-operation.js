9b2dad3a03da5082b55102880ed481c11519b782
sohagroy7223 <freelancersohagroy@gmail.com>
2025-11-18 22:22:52 +0600

    addad for loop and while loop in function

--- array-operation.js
+++ array-operation.js
@@ -1,11 +1,26 @@
-function sumOfNumbers(numbers) {
-    let sum = 0;
-    for (const number of numbers) {
-        console.log(number);
-        sum = sum + number;
+// function sumOfNumbers(numbers) {
+//     let sum = 0;
+//     for (const number of numbers) {
+//         console.log(number);
+//         sum = sum + number;
+//     }
+//     return sum;
+// }
+// const num = [13, 4, 8, 2];
+// const sum = sumOfNumbers(num);
+// console.log("the number of sum is", sum);
+
+//
+
+function wloop(numbers) {
+    let i = 0;
+    let result = [];
+    while (i < numbers.length) {
+        result.push(numbers[i]);
+        i++;
     }
-    return sum;
+    return result;
 }
-const num = [13, 4, 8, 2];
-const sum = sumOfNumbers(num);
-console.log("the number of sum is", sum);
+const num = [2, 3, 56, 7, 9];
+const output = wloop(num);
+console.log(output);
